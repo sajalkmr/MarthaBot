24d5686da29a71c685e6fdb544835235a5e7d4cc
Sajal <sajal.kumar2003@gmail.com>
2022-11-01 14:50:37 +0530

    fix error

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - This is an open source project. 
-- Source code: <a href=https://github.com/sajalkmr/JonasDarkBot>GitHub</a>
+- Source code: <a href=https://github.com/sajalkmr/MarthaBot>GitHub</a>
 
 <b>Owner:</b>
 - Sajal : <a href=https://github.com/sajalkmr>GitHub</a>"""
--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/sajalkmr/JonasDarkBot
+  git clone https://github.com/sajalkmr/MarthaBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /JonasDarkBot
+  git clone $UPSTREAM_REPO /MarthaBot
 fi
-cd /JonasBot
+cd /MarthaBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
